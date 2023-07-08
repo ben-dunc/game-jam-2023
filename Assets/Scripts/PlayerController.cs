5bbfd88ae3f74f94d55f9c3e93dd2e297346eb06
Ben Duncan <58539746+ben-dunc@users.noreply.github.com>
2023-07-08 15:22:53 -0600

    Animations and initial scene stuff

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Input;
+
+public class PlayerController : MonoBehaviour {
+    
+    void Start() {
+        
+    }
+
+    void Update() {
+        // if ()
+    }
+}
